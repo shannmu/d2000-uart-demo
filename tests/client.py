2bdb2b147f1add7099851114201ce6a3a71aaed6
shannmu <shanmu1901@gmail.com>
2024-12-26 01:36:23 +0800

    fix: make it function

--- tests/client.py
+++ tests/client.py
@@ -2,34 +2,30 @@ import serial
 import struct
 import time
 
-# 串口设置
-port = "/dev/ttyUSB0"  # 串口设备路径，根据实际修改
-baud_rate = 115200     # 波特率
-timeout = 5            # 读取超时时间
-
-# 打开串口
-ser = serial.Serial(port, baud_rate, timeout=timeout)
-
-# 发送数据函数
-def send_data(n):
-    # 将n转化为大端格式的四字节
-    data = struct.pack('>I', n)  # '>I' 表示将整数打包成大端格式的4字节
+def get_result_from_non_root_cell(tid: int):
+    if not isinstance(tid, int):
+        raise
+    
+    # 串口设置
+    port = "/dev/ttyUSB0"  # 串口设备路径，根据实际修改
+    baud_rate = 115200     # 波特率
+    timeout = 5            # 读取超时时间
+    
+    ser = serial.Serial(port, baud_rate, timeout=timeout)
+    
+     # 将n转化为大端格式的四字节
+    data = struct.pack('>I', tid)  # '>I' 表示将整数打包成大端格式的4字节
     # 发送 0xeb 0x90 n（4字节）
     ser.write(bytes([0xeb, 0x90]) + data)
-
-# 测试发送数据
-try:
-    while True:
-        # 发送一个测试的n值
-        n = 2  # 你可以修改这个值
-        send_data(n)
-        print(f"Sent: 0xeb 0x90 {n:#010x}")
-        time.sleep(2)
-        # 接收数据
-        recv = ser.read(6)
-        print(f"Received: {recv}")
-except KeyboardInterrupt:
-    print("Test interrupted by user.")
-finally:
+    ser.flush()
+    
+    recv = ser.read(6)
+    # 过滤特殊标记字符0xeb0x90
+    recv = recv[2:]
+    # recv剩余四字节, 大端法转化为int
+    res = struct.unpack('>I', recv)[0]
+    
+    
     ser.close()
-
+    
+    return res
